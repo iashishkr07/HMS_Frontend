0a990f02dc76465b108837539170ff793cefbfa9
Ashish Singh <159794249+iashishkr07@users.noreply.github.com>
2025-05-29 18:45:33 +0530

    Update Home1.jsx

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Header1 from '../components/Header1'
 import ServicesInfo from '../components/ServicesInfo'
-import ServicesSection from '../components/ServicesSection'
 import Testimonials from '../components/Testimonials'
-import TopDoctors1 from '../components/TopDoctors1'
+import TopDoctors from '../components/TopDoctors'
 import Emergency from './Emergency'
 import AboutUs1 from '../components/AboutUs1'
 import Navbar from '../components/Navbar'
@@ -14,8 +13,7 @@ const Home1 = () => {
             <Navbar />
             <Header1 />
             <ServicesInfo />
-            <ServicesSection />
-            <TopDoctors1 />
+            <TopDoctors />
             <AboutUs1 />
             <Emergency />
             <Testimonials />
