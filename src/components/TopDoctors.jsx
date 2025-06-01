69060fd7e3d128c88288c594487cacc7105214e5
Ashish Singh <159794249+iashishkr07@users.noreply.github.com>
2025-06-01 11:03:29 +0530

    Update TopDoctors.jsx

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -41,7 +41,7 @@ const TopDoctors = () => {
       <p className="w-full md:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
-      <div className="w-full md:w-[80%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-5 gap-y-6">
+      <div className="w-full md:w-[80%] grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-4 pt-5 gap-y-4 sm:gap-y-6">
         {Array.isArray(doctors) &&
           doctors.slice(0, 8).map((item, index) => (
             <div
@@ -49,17 +49,21 @@ const TopDoctors = () => {
               className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[10px] transition-all duration-500"
             >
               <img
-                className="w-full object-contain h-48"
+                className="w-full object-contain h-32 sm:h-48"
                 src={item.image}
                 alt={item.name}
               />
-              <div className="p-4">
-                <div className="flex items-center gap-2 text-sm text-center text-green-500">
+              <div className="p-2 sm:p-4">
+                <div className="flex items-center gap-2 text-xs sm:text-sm text-center text-green-500">
                   <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                   <p>Available</p>
                 </div>
-                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
-                <p className="text-gray-600 text-sm">{item.speciality}</p>
+                <p className="text-gray-900 text-base sm:text-lg font-medium">
+                  {item.name}
+                </p>
+                <p className="text-gray-600 text-xs sm:text-sm">
+                  {item.speciality}
+                </p>
               </div>
             </div>
           ))}
