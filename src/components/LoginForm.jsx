60d5ace619d62eaa2aee5d94d4ebee91edef205d
Sapna817 <110749363+Sapna817@users.noreply.github.com>
2025-06-02 18:50:42 +0530

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -20,7 +20,7 @@ const LoginForm = () => {
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       navigate('/1');
-      window.location.reload();
+      // window.location.reload();
     } catch (err) {
       setErrorMessage(err.response?.data?.message || 'Login failed');
     }
