8c0e0d5178f93d37532acee863fb3fbee584500c
Sapna817 <110749363+Sapna817@users.noreply.github.com>
2025-06-15 18:12:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -162,7 +162,7 @@ const Navbar = () => {
           </li>
         ) : (
           <li>
-            <Link to="/loged" onClick={() => setMobileMenuOpen(false)}>
+            <Link to="/1" onClick={() => setMobileMenuOpen(false)}>
               Home
             </Link>
           </li>
@@ -279,7 +279,7 @@ const Navbar = () => {
           </li>
         ))}
         <li>
-          <Link to="/contact" onClick={() => setMobileMenuOpen(false)}>
+          <Link to="/Contact" onClick={() => setMobileMenuOpen(false)}>
             Contact Us
           </Link>
         </li>
