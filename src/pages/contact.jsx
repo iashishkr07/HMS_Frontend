a463f107426b0db6d942cf16685a892fcfdc8573
Sapna817 <110749363+Sapna817@users.noreply.github.com>
2025-06-15 18:03:34 +0530

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,1 +1,12 @@
+import Navbar from '../components/Navbar'
+import Contact from '../components/ContactUs'
+const contact = () => {
+  return (
+    <>
+    <Navbar />
+    <Contact/>
+    </>
+  )
+}
 
+export default contact
